592d72a3e34452ac4269b320ce2081ae011ecc07
ranof11 <rajeshtriadi14@gmail.com>
2025-05-25 19:09:58 +0700

    Initial Commit

---
+++ WalletCardAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletCardAnimation
+//
+//  Created by Rajesh Triadi Noftarizal on 25/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletCardAnimation/WalletCardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletCardAnimationApp.swift
+//  WalletCardAnimation
+//
+//  Created by Rajesh Triadi Noftarizal on 25/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WalletCardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
